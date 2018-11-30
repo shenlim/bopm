bb8f9072a9b5462f26880edb04e7e0237903f690
Shen <lim.shen@outlook.com>
2018-11-29 18:53:08 -0600

    Merged functions, incorporated proper styling practices

--- bopm.R
+++ bopm.R
@@ -1,82 +1,59 @@
 # Binomial Options Pricing Model - Oct 2018. Author: Shen Lim
-# Copyright 2018, Shen Lim, All Rights Reserved.
 
-p_prob <- function(r, delta_t, sigma, div) {
-  u = exp(sigma*sqrt(delta_t))
-  d = (1/u)
-  return((exp((r-div)*delta_t)-d)/(u-d))
-}
-
-tree_stock <- function(S, N, delta_t, sigma) {
-  tree = matrix(NA, nrow=N+1, ncol=N+1)
-  u = exp(sigma*sqrt(delta_t))
-  d = (1/u)
-
-  for (i in 1:(N+1)) {
+bopm <- function(s, k, r, t, n, sigma, type, style, div) {
+  # Computes option price based on binomial tree (discrete-time) approach (Cox, Ross, Rubinstein 1979).
+  #
+  # Args:
+  #   s:     Current underlying asset price.---------------------[numeric]
+  #   k:     Option strike price.--------------------------------[numeric]
+  #   r:     Risk-free rate of interest expressed in decimals.---[numeric]
+  #   t:     Time period expressed in years.---------------------[numeric]
+  #   n:     Number of steps.------------------------------------[integer]
+  #   sigma: Underlying asset price volatility.------------------[numeric]
+  #   type:  "call" or "put".------------------------------------[character]
+  #   style: "e" for European options, "a" for American options.-[character]
+  #   div:   Dividend yield expressed in decimals; "0" if none.--[numeric]
+  #
+  # Returns and stores:
+  #   (1) Risk neutral probability, (2) Stock tree, (3) Exercise payoff tree,
+  #   (4) Option tree, (5) Option delta, (6) Option price at t = 0.
+  
+  u <- exp(sigma * sqrt(t / n))
+  d <- 1 / u
+  p <<- (exp((r - div) * t / n) - d) / (u - d)
+  
+  tree_stock <- matrix(NA, nrow = n + 1, ncol = n + 1)
+  for (i in 1:(n + 1)) {
     for (j in 1:i) {
-      tree[i,j] = S*u^(j-1)*d^(i-j)
+      tree_stock[i, j] <- s * u^(j - 1) * d^(i - j)
     }
   }
-  return(tree)
-}
-
-exercise <- function(S, K, N, delta_t, sigma, type) {
-  ex = matrix(NA, nrow=N+1, ncol=N+1)
-  u = exp(sigma*sqrt(delta_t))
-  d = (1/u)
-
-  if(type == "call") {
-    for (i in 1:(N+1)) {
-      for (j in 1:i) {
-        ex[i,j] = max(((S*u^(j-1)*d^(i-j))-K),0)
+  tree_stock <<- tree_stock
+  
+  tree_exercise <- matrix(NA, nrow = n + 1, ncol = n + 1)
+  for (i in 1:(n + 1)) {
+    for (j in 1:i) {
+      tree_exercise[i, j] <- if (type == "call") max(tree_stock[i, j] - k, 0) else max(k - tree_stock[i, j], 0)
+    }
+  }
+  tree_exercise <<- tree_exercise
+  
+  tree_option <- matrix(NA, nrow = n + 1, ncol = n + 1)
+  tree_option[n + 1, ] <- if (type == "call") pmax(tree_stock[n + 1, ] - k, 0) else pmax(k - tree_stock[n + 1, ], 0)
+  for (i in n:1) {
+    for (j in 1:i) {
+      if (style == "a") {
+        exercise_payoff <- if (type == "call") max(tree_stock[i, j] - k, 0) else max(k - tree_stock[i, j], 0)
+        hold_payoff <- (p * tree_option[i + 1, j + 1] + (1 - p) * tree_option[i + 1, j]) / exp(r * t / n)
+        tree_option[i, j] <- max(exercise_payoff, hold_payoff)
+      } else {
+        tree_option[i, j] <- (p * tree_option[i + 1, j + 1] + (1 - p) * tree_option[i + 1, j]) / exp(r * t / n)
       }
     }
-    return(ex)
   }
-  else {
-    for (i in 1:(N+1)) {
-      for (j in 1:i) {
-        ex[i,j] = max((K-(S*u^(j-1)*d^(i-j))),0)
-      }
-    }
-    return(ex)
-  }
-}
-
-value_binomial_option <- function(K, r, delta_t, sigma, type, american, tree, ex, div) {
-  p = p_prob(r, delta_t, sigma, div)
-  option_tree = matrix(NA, nrow=nrow(tree), ncol=ncol(tree))
-  if(type == "call") {
-    option_tree[nrow(option_tree),] = pmax(tree[nrow(tree),]-K,0)
-  }
-  else {
-    option_tree[nrow(option_tree),] = pmax(K-tree[nrow(tree),],0)
-  }
-  if (american == TRUE) {
-    for (i in (nrow(tree)-1):1) {
-      for(j in 1:i) {
-        exercise.payoff <- if (type == "call") max(tree[i,j] - K, 0) else max(K - tree[i,j], 0)
-        hold.payoff <- (p*option_tree[i+1,j+1] + (1-p)*option_tree[i+1,j])/exp(r*delta_t)
-        option_tree[i,j] <- max(exercise.payoff, hold.payoff)
-      }
-    }
-    return(option_tree)
-  }
-  else {
-    for (i in (nrow(tree)-1):1) {
-      for(j in 1:i) {
-        option_tree[i,j] = (p*option_tree[i+1,j+1] + (1-p)*option_tree[i+1,j])/exp(r*delta_t)
-      }
-    }
-    return(option_tree)
-  }
-}
-
-binomial_option <- function(S, K, r, T, N, sigma, type, american, div) {
-  p = p_prob(r= r, delta_t= T/N, sigma= sigma, div= div)
-  tree = tree_stock(S= S, N= N, delta_t= T/N, sigma= sigma)
-  ex = exercise(S= S, K= K, N= N, delta_t= T/N, sigma= sigma, type= type)
-  option = value_binomial_option(K= K, r= r, delta_t= T/N, sigma= sigma, type= type, american, tree, ex, div)
-  delta = (option[2,2]-option[2,1])/(tree[2,2]-tree[2,1])
-  return(list(p= p, delta= delta, stock= tree, exercise= ex, option= option, price= option[1,1]))
+  tree_option <<- tree_option
+  
+  delta <<- (tree_option[2, 2] - tree_option[2, 1]) / (tree_stock[2, 2] - tree_stock[2, 1])
+  price <<- tree_option[1, 1]
+  return(list(p = p, stock_tree = tree_stock, exercise_tree = tree_exercise, option_tree = tree_option, delta = delta, price = price))
 }
